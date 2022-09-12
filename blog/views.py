460c85edcff5db2a9747e3696b4210d92e5e7ec5
leeny_the_bear <leeny@inbox.ru>
2022-09-12 23:29:37 +0700

    move all queries for most popular posts into QuerySet methods

--- blog/views.py
+++ blog/views.py
@@ -82,20 +82,7 @@ def post_detail(request, slug):
 
     most_popular_tags = Tag.objects.popular()[:5]
 
-    most_popular_posts = (
-        Post.objects.prefetch_related("author")
-        .annotate(
-            likes_count=Count("likes", distinct=True)).order_by(
-            "-likes_count")[:5]
-    )
-    most_popular_posts_ids = [post.id for post in most_popular_posts]
-    posts_with_comments = Post.objects.filter(
-        id__in=most_popular_posts_ids).annotate(
-        comments_count=Count('comments'))
-    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
-    count_for_id = dict(ids_and_comments)
-    for post in most_popular_posts:
-        post.comments_count = count_for_id[post.id]
+    most_popular_posts = Post.objects.popular().prefetch_related('author')[:5].fetch_with_comments_count()
 
     context = {
         "post": serialized_post,
@@ -112,20 +99,7 @@ def tag_filter(request, tag_title):
 
     most_popular_tags = Tag.objects.popular()[:5]
 
-    most_popular_posts = (
-        Post.objects.prefetch_related("author")
-        .annotate(
-            likes_count=Count("likes")).order_by(
-            "-likes_count")[:5]
-    )
-    most_popular_posts_ids = [post.id for post in most_popular_posts]
-    posts_with_comments = Post.objects.filter(
-        id__in=most_popular_posts_ids).annotate(
-        comments_count=Count('comments'))
-    ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
-    count_for_id = dict(ids_and_comments)
-    for post in most_popular_posts:
-        post.comments_count = count_for_id[post.id]
+    most_popular_posts = Post.objects.popular().prefetch_related('author')[:5].fetch_with_comments_count()
 
     related_posts = tag.posts.all().annotate(
             likes_count=Count("likes"))[:20]
