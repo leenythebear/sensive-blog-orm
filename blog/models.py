8ea1ac24b3116b29a480674224c6c8ce3107020a
leeny_the_bear <leeny@inbox.ru>
2022-09-14 19:39:07 +0700

    format code

--- blog/models.py
+++ blog/models.py
@@ -14,7 +14,9 @@ class PostQuerySet(models.QuerySet):
         posts_with_comments = Post.objects.filter(
             id__in=most_popular_posts_ids
         ).annotate(comments_count=Count("comments"))
-        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        ids_and_comments = posts_with_comments.values_list(
+            "id", "comments_count"
+        )
         count_for_id = dict(ids_and_comments)
         for post in self:
             post.comments_count = count_for_id[post.id]
@@ -22,38 +24,39 @@ class PostQuerySet(models.QuerySet):
 
 
 class Post(models.Model):
-    title = models.CharField('Заголовок', max_length=200)
-    text = models.TextField('Текст')
-    slug = models.SlugField('Название в виде url', max_length=200)
-    image = models.ImageField('Картинка')
-    published_at = models.DateTimeField('Дата и время публикации')
+    title = models.CharField("Заголовок", max_length=200)
+    text = models.TextField("Текст")
+    slug = models.SlugField("Название в виде url", max_length=200)
+    image = models.ImageField("Картинка")
+    published_at = models.DateTimeField("Дата и время публикации")
 
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        verbose_name='Автор',
-        limit_choices_to={'is_staff': True})
+        verbose_name="Автор",
+        limit_choices_to={"is_staff": True},
+    )
     likes = models.ManyToManyField(
         User,
-        related_name='liked_posts',
-        verbose_name='Кто лайкнул',
-        blank=True)
+        related_name="liked_posts",
+        verbose_name="Кто лайкнул",
+        blank=True,
+    )
     tags = models.ManyToManyField(
-        'Tag',
-        related_name='posts',
-        verbose_name='Теги')
+        "Tag", related_name="posts", verbose_name="Теги"
+    )
     objects = PostQuerySet.as_manager()
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('post_detail', args={'slug': self.slug})
+        return reverse("post_detail", args={"slug": self.slug})
 
     class Meta:
-        ordering = ['-published_at']
-        verbose_name = 'пост'
-        verbose_name_plural = 'посты'
+        ordering = ["-published_at"]
+        verbose_name = "пост"
+        verbose_name_plural = "посты"
 
 
 class TagQuerySet(models.QuerySet):
@@ -75,33 +78,32 @@ class Tag(models.Model):
         self.title = self.title.lower()
 
     def get_absolute_url(self):
-        return reverse('tag_filter', args={'tag_title': self.slug})
+        return reverse("tag_filter", args={"tag_title": self.slug})
 
     class Meta:
-        ordering = ['title']
-        verbose_name = 'тег'
-        verbose_name_plural = 'теги'
+        ordering = ["title"]
+        verbose_name = "тег"
+        verbose_name_plural = "теги"
 
 
 class Comment(models.Model):
     post = models.ForeignKey(
-        'Post',
+        "Post",
         on_delete=models.CASCADE,
-        verbose_name='Пост, к которому написан',
-        related_name='comments',
+        verbose_name="Пост, к которому написан",
+        related_name="comments",
     )
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        verbose_name='Автор')
+        User, on_delete=models.CASCADE, verbose_name="Автор"
+    )
 
-    text = models.TextField('Текст комментария')
-    published_at = models.DateTimeField('Дата и время публикации')
+    text = models.TextField("Текст комментария")
+    published_at = models.DateTimeField("Дата и время публикации")
 
     def __str__(self):
-        return f'{self.author.username} under {self.post.title}'
+        return f"{self.author.username} under {self.post.title}"
 
     class Meta:
-        ordering = ['published_at']
-        verbose_name = 'комментарий'
-        verbose_name_plural = 'комментарии'
+        ordering = ["published_at"]
+        verbose_name = "комментарий"
+        verbose_name_plural = "комментарии"
