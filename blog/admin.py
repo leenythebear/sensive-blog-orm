c832eb875cdbc850cb27c3112ada175609deadde
leeny_the_bear <leeny@inbox.ru>
2022-09-15 23:07:52 +0700

    add raw id fields

--- blog/admin.py
+++ blog/admin.py
@@ -6,13 +6,14 @@ from blog.models import Post, Tag, Comment
 class PostAdmin(admin.ModelAdmin):
     list_display = (
         'title', 'author', 'published_at')
-    raw_id_fields = ['likes', 'tags']
+    raw_id_fields = ['likes', 'tags', 'author']
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = (
         'post', 'author', 'published_at')
+    raw_id_fields = ['post', 'author']
 
 
 @admin.register(Tag)
