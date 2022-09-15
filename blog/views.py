4049fe401c7f99f6de60686e9f9dd141c2bd71d9
leeny_the_bear <leeny@inbox.ru>
2022-09-15 20:58:51 +0700

    rename function

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from blog.models import Comment, Post, Tag
 
 
-def serialize_post_optimized(post):
+def serialize_post(post):
     return {
         "title": post.title,
         "teaser_text": post.text[:200],
@@ -33,10 +33,10 @@ def index(request):
 
     context = {
         "most_popular_posts": [
-            serialize_post_optimized(post) for post in most_popular_posts
+            serialize_post(post) for post in most_popular_posts
         ],
         "page_posts": [
-            serialize_post_optimized(post) for post in most_fresh_posts
+            serialize_post(post) for post in most_fresh_posts
         ],
         "popular_tags": [serialize_tag(tag) for tag in most_popular_tags],
     }
@@ -78,7 +78,7 @@ def post_detail(request, slug):
         "post": serialized_post,
         "popular_tags": [serialize_tag(tag) for tag in most_popular_tags],
         "most_popular_posts": [
-            serialize_post_optimized(post) for post in most_popular_posts
+            serialize_post(post) for post in most_popular_posts
         ],
     }
     return render(request, "post-details.html", context)
@@ -96,9 +96,9 @@ def tag_filter(request, tag_title):
     context = {
         "tag": tag.title,
         "popular_tags": [serialize_tag(tag) for tag in most_popular_tags],
-        "posts": [serialize_post_optimized(post) for post in related_posts],
+        "posts": [serialize_post(post) for post in related_posts],
         "most_popular_posts": [
-            serialize_post_optimized(post) for post in most_popular_posts
+            serialize_post(post) for post in most_popular_posts
         ],
     }
     return render(request, "posts-list.html", context)
